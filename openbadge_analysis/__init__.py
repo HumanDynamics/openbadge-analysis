924d4267deb53aed08c7ecf00ce01e878972f4ae
Oren Lederman <oren.lederman@gmail.com>
2017-12-14 21:58:08 -0500

    Bump version: 0.3.5 → 0.3.6

--- openbadge_analysis/__init__.py
+++ openbadge_analysis/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Oren Lederman'
 __email__ = 'orenled@mit.edu'
-__version__ = '0.3.5'
+__version__ = '0.3.6'
 
 __all__ = ["core", "data", "preprocessing", "visualization", "analysis"]
 
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='openbadge_analysis',
-    version='0.3.5',
+    version='0.3.6',
     description="Python libraries for analysing OpenBadge data",
     long_description=readme + '\n\n' + history,
     author="Oren Lederman",
