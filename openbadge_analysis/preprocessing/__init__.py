55c22659e5ea7a983580e564df10504f4212a915
Jiaxing Shen <jxshen.polyu@gmail.com>
2018-04-11 13:51:23 -0400

    new_vad stuff

--- openbadge_analysis/preprocessing/__init__.py
+++ openbadge_analysis/preprocessing/__init__.py
@@ -2,7 +2,6 @@
 
 from .raw import split_raw_data_by_day
 
-
 from .metadata import id_to_member_mapping
 from .metadata import voltages
 from .metadata import sample_counts
@@ -18,3 +17,7 @@ from .proximity import member_to_beacon_proximity_fill_gaps
 from .hublog import hublog_scans
 from .hublog import hublog_resets
 from .hublog import hublog_clock_syncs
+
+from .audio import *
+
+
--- openbadge_analysis/visualization/__init__.py
+++ openbadge_analysis/visualization/__init__.py
@@ -3,3 +3,4 @@
 
 from .embedding import rssi_to_distance, members_2d_embedding, members_2d_embedding_with_beacons
 
+from .audio_visualization import *
