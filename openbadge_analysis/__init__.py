6340a2259791bc14369768b9b10595b759788cbe
Oren Lederman <oren.lederman@gmail.com>
2019-04-16 15:14:52 -0700

    Bump version: 0.4.1 → 0.4.2

--- openbadge_analysis/__init__.py
+++ openbadge_analysis/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Oren Lederman'
 __email__ = 'orenled@mit.edu'
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 
 __all__ = ["core", "data", "preprocessing", "visualization", "analysis"]
 
